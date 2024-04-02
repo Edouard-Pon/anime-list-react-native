d4532143c7cd2aa4c0cbc61da617cb6cf67a2235
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-04-02 03:31:42 +0200

    feat: Implement universal navigation menu across all pages

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -4,16 +4,37 @@ import { createStackNavigator } from '@react-navigation/stack';
 import MainPage from '../screens/MainPage';
 import AnimePage from '../screens/AnimePage';
 import CharacterPage from '../screens/CharacterPage';
+import CharactersPage from '../screens/CharactersPage';
+import { Menu, Button } from 'react-native-paper';
+import { useDispatch, useSelector } from 'react-redux';
+import { openMenu, closeMenu } from '../store/menu';
 
 const Stack = createStackNavigator();
 
 function AppNavigator() {
+  const dispatch = useDispatch();
+  const isMenuOpen = useSelector((state) => state.menu.isOpen);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={({ navigation }) => ({
+          headerRight: () => (
+            <Menu
+              visible={isMenuOpen}
+              onDismiss={() => dispatch(closeMenu())}
+              anchor={<Button onPress={() => dispatch(openMenu())}>Menu</Button>}
+            >
+              <Menu.Item onPress={() => { navigation.navigate('MainPage'); dispatch(closeMenu()); }} title="MainPage" />
+              <Menu.Item onPress={() => { navigation.navigate('CharactersPage'); dispatch(closeMenu()); }} title="Characters" />
+            </Menu>
+          ),
+        })}
+      >
         <Stack.Screen name="MainPage" component={MainPage} />
         <Stack.Screen name="AnimePage" component={AnimePage} />
         <Stack.Screen name="CharacterPage" component={CharacterPage} />
+        <Stack.Screen name="CharactersPage" component={CharactersPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
