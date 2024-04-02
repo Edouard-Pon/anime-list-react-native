aeb1dd4244699f5ce97e1b646e113c91c3615fa2
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-04-03 01:09:58 +0200

    feat: Implement user login functionality
    - Implement login screen UI
    - Add API call in user.js to authenticate user
    - Store JWT token in AsyncStorage upon successful login

---
+++ src/store/user.js
@@ -0,0 +1,27 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import api from '../services/api';
+
+export const login = createAsyncThunk('user/login', async ({ username, password }) => {
+  try {
+    const response = await api.post('/user/login', { username, password });
+    await AsyncStorage.setItem('token', response.data.token);
+    return response.data.success;
+  } catch (error) {
+    return error.response.data.message;
+  }
+});
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    userInfo: null,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.userInfo = action.payload;
+    });
+  },
+});
+
+export default userSlice.reducer;
