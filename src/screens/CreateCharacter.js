fe29a13fe776ac4237da0edb2d61d833d276177f
YehorKuzmenko <yehor.kuzmenko@etu.univ-amu.fr>
2024-04-03 20:41:22 +0200

    Add style for character create page

--- src/screens/CreateCharacter.js
+++ src/screens/CreateCharacter.js
@@ -46,43 +46,60 @@ export default function CreateCharacter() {
 
     return (
         <View style={styles.container}>
-            <TextInput
-                style={styles.input}
-                placeholder="Name"
-                value={name}
-                onChangeText={setName}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Original Name"
-                value={originalName}
-                onChangeText={setOriginalName}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Description"
-                value={description}
-                onChangeText={setDescription}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Anime"
-                value={anime}
-                onChangeText={setAnime}
-            />
-            <Button title="Upload cover image" onPress={handleImagePick} />
-            {image ? (
-                <>
-                    <Button title="Clear image" onPress={() => setImage(null)} />
-                    <Image
-                        source={{ uri: image.uri }}
-                        style={styles.image}
-                        onError={(error) => console.error('Image loading error:', error)}
+            <View style={styles.imageAndInputsContainer}>
+                <View style={styles.imageContainer}>
+                    {image ? (
+                        <Image
+                            source={{ uri: image.uri }}
+                            style={styles.image}
+                            onError={(error) => console.error('Image loading error:', error)}
+                        />
+                    ) : (
+                        <View style={styles.noImagePlaceholder}>
+                            <Text>No image selected</Text>
+                        </View>
+                    )}
+                </View>
+                <View style={styles.inputsContainer}>
+                    <TextInput
+                        style={styles.topInput}
+                        placeholder="Name"
+                        value={name}
+                        onChangeText={setName}
                     />
-                </>
-            ) : (
-                <Text>No image selected</Text>
-            )}
+                    <TextInput
+                        style={styles.topInput}
+                        placeholder="Original Name"
+                        value={originalName}
+                        onChangeText={setOriginalName}
+                    />
+                    <TextInput
+                        style={styles.topInput}
+                        placeholder="Anime"
+                        value={anime}
+                        onChangeText={setAnime}
+                    />
+                    <View style={styles.buttonContainer}>
+                        {!image && (
+                            <Button style={styles.button} title="Upload image" onPress={handleImagePick} />
+                        )}
+                        {image && (
+                            <Button style={styles.button} title="Clear image" onPress={() => setImage(null)} />
+                        )}
+                    </View>
+                </View>
+            </View>
+            <View style={styles.descriptionContainer}>
+                <TextInput
+                    style={styles.descriptionInput}
+                    placeholder="Description"
+                    multiline={true}
+                    numberOfLines={4}
+                    value={description}
+                    onChangeText={setDescription}
+                />
+            </View>
+
             <Button title="Create" onPress={handleCreate} />
         </View>
     );
@@ -91,19 +108,60 @@ export default function CreateCharacter() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         padding: 16,
+        backgroundColor: 'white',
+    },
+    imageAndInputsContainer: {
+        flexDirection: 'row',
+        marginBottom: 16,
+    },
+    imageContainer: {
+        marginRight: 16,
+    },
+    noImagePlaceholder: {
+        width: 160,
+        height: 240,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ccc',
+    },
+    image: {
+        width: 160,
+        height: 240,
+        borderRadius: 10,
+    },
+    inputsContainer: {
+        flex: 1,
     },
     input: {
         height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginBottom: 16,
+        borderBottomColor: 'black',
+        borderBottomWidth: 3,
         paddingLeft: 8,
+        marginBottom: 16,
     },
-    image: {
-        width: 200,
-        height: 200,
-        marginVertical: 10,
+    topInput: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 3,
+        marginBottom: 20,
+        marginTop: 10,
+    },
+    buttonContainer: {
+        marginTop: 10,
+        marginBottom: 16,
+    },
+    button: {
+        height: 60,
+    },
+    descriptionContainer: {
+        marginBottom: 16,
+    },
+    descriptionInput: {
+        height: 120,
+        borderWidth: 3,
+        padding: 8,
+        textAlignVertical: 'top',
     },
 });
