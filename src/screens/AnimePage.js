b7e2c461fb11ebfc953fd66799ca3de2a5a95008
YehorKuzmenko <yehor.kuzmenko@etu.univ-amu.fr>
2024-04-03 20:57:39 +0200

    Add style for single anime page and searchbar in main

--- src/screens/AnimePage.js
+++ src/screens/AnimePage.js
@@ -4,38 +4,57 @@ import {View, Text, StyleSheet, Image} from 'react-native';
 function AnimePage({ route }) {
   const { anime } = route.params;
 
-  return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={{uri: anime.coverImagePath}} />
-      <Text style={styles.title}>{anime.title}</Text>
-      <Text>Type: {anime.type}</Text>
-      <Text>Episodes: {anime.episodes}</Text>
-      <Text>Status: {anime.status}</Text>
-      <Text>Description: {anime.description}</Text>
-      <Text>Release Date: {new Date(anime.releaseDate).toLocaleDateString('en-GB')}</Text>
-      <Text>Upload Date: {new Date(anime.uploadDate).toLocaleDateString('en-GB')}</Text>
-      <Text>Source: {anime.source}</Text>
-      <Text>External Link: {anime.externalLink}</Text>
-      <Text>Duration: {anime.duration}</Text>
-      <Text>Rating: {anime.rating}</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{anime.title}</Text>
+            <View style={styles.imageAndTextContainer}>
+                <Image style={styles.image} source={{ uri: anime.coverImagePath }} />
+                <View style={styles.textContainer}>
+                    <Text style={styles.text}>Type: {anime.type}</Text>
+                    <Text style={styles.text}>Episodes: {anime.episodes}</Text>
+                    <Text style={styles.text}>Duration: {anime.duration}</Text>
+                    <Text style={styles.text}>Release Date: {new Date(anime.releaseDate).toLocaleDateString('en-GB')}</Text>
+                    <Text style={styles.text}>Upload Date: {new Date(anime.uploadDate).toLocaleDateString('en-GB')}</Text>
+                </View>
+            </View>
+            <Text style={styles.text}>Status: {anime.status}</Text>
+            <Text style={styles.text}>Rating: {anime.rating}</Text>
+            <Text style={styles.text}>Description: {anime.description}</Text>
+            <Text style={styles.text}>Source: {anime.source}</Text>
+            <Text style={styles.text}>External Link: {anime.externalLink}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  image: {
-    width: 100,
-    height: 100,
-  },
+    container: {
+        flex: 1,
+        padding: 16,
+
+    },
+    imageAndTextContainer: {
+        flexDirection: 'row',
+        marginBottom: 16,
+        alignItems: 'flex-start',
+    },
+    image: {
+        width: 160,
+        height: 240,
+    },
+    textContainer: {
+        marginLeft: 16,
+        flex: 1,
+        justifyContent: 'flex-start',
+    },
+    text: {
+      fontSize: 18,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 20,
+        marginBottom: 30
+    },
 });
 
 export default AnimePage;
