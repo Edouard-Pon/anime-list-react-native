1ded19f02d2fe981b4bd52d234314c49b6622177
YehorKuzmenko <yehor.kuzmenko@etu.univ-amu.fr>
2024-04-04 09:01:12 +0200

    feat: Add bottom navigation menu

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,60 +1,75 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import MainPage from '../screens/MainPage';
-import AnimePage from '../screens/AnimePage';
-import CharacterPage from '../screens/CharacterPage';
 import CharactersPage from '../screens/CharactersPage';
 import CreateAnimePage from '../screens/CreateAnime';
 import CreateCharacterPage from '../screens/CreateCharacter';
 import LoginPage from '../screens/LoginPage';
 import SignupPage from '../screens/SignupPage';
 import ProfilePage from '../screens/ProfilePage';
-
-import { Menu, Button } from 'react-native-paper';
-import { useDispatch, useSelector } from 'react-redux';
-import { openMenu, closeMenu } from '../store/menu';
+import AnimePage from '../screens/AnimePage';
+import CharacterPage from '../screens/CharacterPage';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function AppNavigator() {
-  const dispatch = useDispatch();
-  const isMenuOpen = useSelector((state) => state.menu.isOpen);
+    return (
+        <NavigationContainer>
+            <SafeAreaView style={{ flex: 1 }}>
+                <Tab.Navigator
+                    screenOptions={({ route }) => ({headerShown: false,
+                        tabBarIcon: ({ focused, color, size }) => {
+                            let iconName;
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={({ navigation }) => ({
-          headerRight: () => (
-            <Menu
-              visible={isMenuOpen}
-              onDismiss={() => dispatch(closeMenu())}
-              anchor={<Button onPress={() => dispatch(openMenu())}>Menu</Button>}
-            >
-              <Menu.Item onPress={() => { navigation.navigate('MainPage'); dispatch(closeMenu()); }} title="MainPage" />
-              <Menu.Item onPress={() => { navigation.navigate('LoginPage'); dispatch(closeMenu()); }} title="Login" />
-              <Menu.Item onPress={() => { navigation.navigate('SignupPage'); dispatch(closeMenu()); }} title="Signup" />
-              <Menu.Item onPress={() => { navigation.navigate('CharactersPage'); dispatch(closeMenu()); }} title="Characters" />
-              <Menu.Item onPress={() => { navigation.navigate('CreateAnime'); dispatch(closeMenu()); }} title="Create Anime" />
-              <Menu.Item onPress={() => { navigation.navigate('CreateCharacter'); dispatch(closeMenu()); }} title="Create Character" />
-              <Menu.Item onPress={() => { navigation.navigate('Profile'); dispatch(closeMenu()); }} title="Profile" />
-            </Menu>
-          ),
-        })}
-      >
-        <Stack.Screen name="MainPage" component={MainPage} />
-        <Stack.Screen name="AnimePage" component={AnimePage} />
-        <Stack.Screen name="CharacterPage" component={CharacterPage} />
-        <Stack.Screen name="CharactersPage" component={CharactersPage} />
-        <Stack.Screen name="CreateAnime" component={CreateAnimePage} />
-        <Stack.Screen name="CreateCharacter" component={CreateCharacterPage} />
-        <Stack.Screen name="LoginPage" component={LoginPage} />
-        <Stack.Screen name="SignupPage" component={SignupPage} />
-        <Stack.Screen name="Profile" component={ProfilePage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+                            if (route.name === 'MainPage') {
+                                iconName = focused ? 'home' : 'home-outline';
+                            } else if (route.name === 'CharactersPage') {
+                                iconName = focused ? 'people' : 'people-outline';
+                            } else if (route.name === 'CreateAnime') {
+                                iconName = focused ? 'add-circle' : 'add-circle-outline';
+                            } else if (route.name === 'CreateCharacter') {
+                                iconName = focused ? 'person-add' : 'person-add-outline';
+                            } else if (route.name === 'Profile') {
+                                iconName = focused ? 'person' : 'person-outline';
+                            }
+
+                            return <Icon name={iconName} size={size} color={color} />;
+                        },
+                        tabBarLabel: '',
+                    })}
+                    tabBarOptions={{
+                        headerShown: false,
+                        activeTintColor: 'pink',
+                        inactiveTintColor: 'gray',
+                        style: {
+                            backgroundColor: '#d2a5ac',
+                        },
+                        labelStyle: {
+                            fontSize: 16,
+                            fontWeight: 'bold',
+                            display: 'none',
+                        },
+                    }}
+                >
+                    <Tab.Screen name="MainPage" component={MainPage} />
+                    <Tab.Screen name="CharactersPage" component={CharactersPage} />
+                    <Tab.Screen name="CreateAnime" component={CreateAnimePage} />
+                    <Tab.Screen name="CreateCharacter" component={CreateCharacterPage} />
+                    <Tab.Screen name="Profile" component={ProfilePage} />
+                    <Stack.Screen name="AnimePage" component={AnimePage} options={{ tabBarButton: () => null }} />
+                    <Stack.Screen name="CharacterPage" component={CharacterPage} options={{ tabBarButton: () => null }} />
+                    <Stack.Screen name="LoginPage" component={LoginPage} options={{ tabBarButton: () => null }}/>
+                    <Stack.Screen name="SignupPage" component={SignupPage} options={{ tabBarButton: () => null }}/>
+                </Tab.Navigator>
+            </SafeAreaView>
+        </NavigationContainer>
+    );
 }
 
 export default AppNavigator;
