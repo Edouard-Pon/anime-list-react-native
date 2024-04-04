7b7260b59562a8beabf45fd547db1ebba851c00a
Edouard-Pon <edouard.ponomarenko@gmail.com>
2024-04-04 03:34:35 +0200

    feat: implement create anime functionality

--- src/screens/CreateAnime.js
+++ src/screens/CreateAnime.js
@@ -1,21 +1,39 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import { ScrollView, Text, StyleSheet, TextInput, Button, Image } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import DropDownPicker from 'react-native-dropdown-picker';
 import * as ImagePicker from 'expo-image-picker';
+import { useDispatch, useSelector } from 'react-redux';
+import { createAnime, setFormData } from '../store/anime';
 
-// Utiliser des DropDownPicker dans un ScrollView peut causer des erreurs askip
-// Pour les ignorer, ajouter le code suivant 
-// Bancal mais t'inquiète 
 import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['VirtualizedLists should never be nested inside plain ScrollViews']);
 
-export default function App() {
-  const [releaseDate, setChosenDate] = useState(new Date());
-  const [showPicker, setShowPicker] = useState(false);
+export default function CreateAnime() {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('');
+  const [type, setType] = useState('');
+  const [episodes, setEpisodes] = useState('');
   const [status, setStatus] = useState('Ongoing');
-  const [rating, setRating] = useState(0);
-  const [errors, setErrors] = useState({});
+  const [description, setDescription] = useState('');
+  const [releaseDate, setReleaseDate] = useState(new Date());
+  const [source, setSource] = useState('');
+  const [externalLink, setExternalLink] = useState('');
+  const [cover, setCover] = useState(null);
+  const [genres, setGenres] = useState([]);
+  const [themes, setThemes] = useState([]);
+  const [duration, setDuration] = useState('');
+  const [rating, setRating] = useState('');
+  const [character, setCharacter] = useState([]);
+
+  const [openRating, setOpenRating] = useState(false);
+  const [valueRating, setValueRating] = useState(null);
+  const [itemsRating, setItemsRating] = useState([
+    { label: 'Family Friendly', value: '13' },
+    { label: 'Teen', value: '16' },
+    { label: 'Mature', value: '18' },
+  ]);
 
   const [openStatus, setOpenStatus] = useState(false);
   const [valueStatus, setValueStatus] = useState(null);
@@ -25,92 +43,9 @@ export default function App() {
     { label: 'Announced', value: 'Announced' },
   ]);
 
-  const [openRating, setOpenRating] = useState(false);
-  const [valueRating, setValueRating] = useState(null);
-  const [itemsRating, setItemsRating] = useState([
-    { label: 'Family Friendly', value: 'Family Friendly' },
-    { label: 'Teen', value: 'Teen' },
-    { label: 'Mature', value: 'Mature' },
-  ]);
-
   const handleDateChange = (event, selectedDate) => {
-    const currentDate = selectedDate || releaseDate;
-    setShowPicker(false);
-    setChosenDate(currentDate);
-  };
-
-  // const validateForm = () => {
-  //   const errors = {};
-  //   if (!formData.title) {
-  //     errors.title = 'Title is required';
-  //   }
-  //   if (!formData.type) {
-  //     errors.type = 'Type is required';
-  //   }
-  //   if (!formData.episodes) {
-  //     errors.episodes = 'Episodes is required';
-  //   }
-  //   if (!formData.description) {
-  //     errors.description = 'Description is required';
-  //   }
-  //   if (!formData.releaseDate) {
-  //     errors.releaseDate = 'Release date is required';
-  //   }
-  //   if (!formData.source) {
-  //     errors.source = 'Source is required';
-  //   }
-  //   if (!formData.externalLink) {
-  //     errors.externalLink = 'External link is required';
-  //   }
-  //   if (!formData.duration) {
-  //     errors.duration = 'Duration is required';
-  //   }
-  //   if (!formData.rating) {
-  //     errors.rating = 'Rating is required';
-  //   }
-  //   if (!formData.coverImage) {
-  //     errors.coverImage = 'Cover image is required';
-  //   }
-  //   setErrors(errors);
-  //   return Object.keys(errors).length === 0;
-  // };
-
-  const showDatePicker = () => {
-    setShowPicker(true);
-  };
-
-  const formatDate = (date) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return date.toLocaleDateString('en-US', options);
-  };
-
-  const handleStatusChange = (selectedStatus) => {
-    setStatus(selectedStatus);
-  };
-
-  const [formData, setFormData] = useState({
-    title: '',
-    type: '',
-    episodes: '',
-    status: '',
-    description: '',
-    releaseDate: '',
-    source: '',
-    externalLink: '',
-    coverImage: null, // Changed to null initially
-    coverImageType: '',
-    genres: '',
-    themes: '',
-    duration: '',
-    rating: '',
-    character: '',
-  });
-
-  const handleChange = (name, value) => {
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+    const currentDate = selectedDate || new Date(releaseDate || Date.now());
+    setReleaseDate(currentDate.toISOString());
   };
 
   const handleImagePick = async () => {
@@ -129,31 +64,39 @@ export default function App() {
     });
 
     if (!result.canceled) {
-      setFormData({
-        ...formData,
-        coverImage: result.assets[0].uri,
-      });
+      setCover(result.assets[0]);
     }
   };
 
-  const handleRatingChange = (selectedRating) => {
-    setRating(selectedRating);
-  };
-
-  const handleSubmit = () => {
-    // Implement logic to handle form submission
+  const handleSubmit = async () => {
+    const formData = {
+      title,
+      type,
+      episodes,
+      status,
+      description,
+      releaseDate,
+      source,
+      externalLink,
+      cover,
+      genres,
+      themes,
+      duration,
+      rating,
+      character,
+    };
+    dispatch(createAnime(formData));
   };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-
       <Text style={styles.title}>Create Anime</Text>
 
-      {formData.coverImage ? (
+      {cover ? (
         <>
-          <Button title="Clear image" onPress={() => handleChange('coverImage', null)} />
+          <Button title="Clear image" onPress={() => setCover(null)} />
           <Image
-            source={{ uri: formData.coverImage }}
+            source={{ uri: cover.uri }}
             style={styles.image}
             onError={(error) => console.error('Image loading error:', error)}
           />
@@ -168,25 +111,25 @@ export default function App() {
       <TextInput
         style={styles.input}
         placeholder="Title"
-        onChangeText={(text) => handleChange('title', text)}
+        onChangeText={setTitle}
         required
       />
       <TextInput
         style={styles.input}
         placeholder="Type"
-        onChangeText={(text) => handleChange('type', text)}
+        onChangeText={setType}
         required
       />
       <TextInput
         style={styles.input}
         placeholder="Episodes"
-        onChangeText={(text) => handleChange('episodes', text)}
+        onChangeText={setEpisodes}
         keyboardType="numeric"
         required
       />
 
-      <Text >Select Status</Text>
       <DropDownPicker
+        placeholder={'Select status'}
         open={openStatus}
         value={valueStatus}
         items={itemsStatus}
@@ -194,49 +137,50 @@ export default function App() {
         setOpen={setOpenStatus}
         setValue={setValueStatus}
         setItems={setItemsStatus}
-        onValueChange={(itemValue, itemIndex) => handleStatusChange(itemValue)}
+        onChangeValue={(itemValue, itemIndex) => {
+          setValueStatus(itemValue);
+          setStatus(itemValue);
+        }}
       />
 
       <TextInput
         style={styles.input}
         placeholder="Description"
-        onChangeText={(text) => handleChange('description', text)}
+        onChangeText={setDescription}
         required
       />
 
-      <Button title="Choose release date" onPress={showDatePicker} />
-      {showPicker && (
-        <DateTimePicker
-          value={releaseDate}
-          mode="date"
-          display="calendar"
-          onChange={handleDateChange}
-        />
-      )}
-      <Text>Release Date: {formatDate(releaseDate)}</Text>
+      <DateTimePicker
+        value={releaseDate ? new Date(releaseDate) : new Date()}
+        mode="date"
+        display="calendar"
+        onChange={handleDateChange}
+      />
+
+      <Text>Release Date: {releaseDate ? new Date(releaseDate).toLocaleDateString() : new Date().toLocaleDateString()}</Text>
 
       <TextInput
         style={styles.input}
         placeholder="Source"
-        onChangeText={(text) => handleChange('source', text)}
+        onChangeText={setSource}
         required
       />
       <TextInput
         style={styles.input}
         placeholder="External Link"
-        onChangeText={(text) => handleChange('externalLink', text)}
+        onChangeText={setExternalLink}
         required
       />
       <TextInput
         style={styles.input}
-        placeholder="Duration (hours)"
-        onChangeText={(text) => handleChange('duration', text)}
+        placeholder="Duration (mins per episode)"
+        onChangeText={setDuration}
         keyboardType="numeric"
         required
       />
 
-      <Text >Select Rating</Text>
       <DropDownPicker
+        placeholder={'Select rating'}
         open={openRating}
         value={valueRating}
         items={itemsRating}
@@ -244,20 +188,23 @@ export default function App() {
         setOpen={setOpenRating}
         setValue={setValueRating}
         setItems={setItemsRating}
-        onValueChange={(itemValue, itemIndex) => handleRatingChange(itemValue)}
+        onChangeValue={(itemValue, itemIndex) => {
+          setValueRating(itemValue);
+          setRating(itemValue);
+        }}
       />
 
-      <Button title="Submit" onPress={handleSubmit} disabled={!formData.title} />
+      <Button title="Submit" onPress={handleSubmit} disabled={!title} />
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1, // Use flexGrow to fill the entire ScrollView
+    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingVertical: 20, // Add padding for spacing
+    paddingVertical: 20,
   },
   title: {
     fontSize: 24,
@@ -270,11 +217,9 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     borderWidth: 1,
     paddingHorizontal: 10,
-    // center the inputs:
     alignSelf: 'center',
   },
   image: {
-    // in a coloured frame
     width: 100,
     height: 100,
     marginVertical: 10,
