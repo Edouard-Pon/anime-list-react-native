9b02f2c4342c12e2ffb28e6b97305a1ae0fbb778
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-10-16 00:23:35 +0200

    feat: add anime card component

---
+++ components/AnimeCard.tsx
@@ -0,0 +1,24 @@
+import { Image } from "react-native";
+import { Link } from "expo-router";
+import { ThemedText } from "@/components/ThemedText";
+
+
+type AnimeCardProps = {
+  id: string;
+  title: string;
+  cover_image: string;
+};
+
+const AnimeCard = (props: AnimeCardProps) => {
+  return (
+    <Link href={{
+      pathname: '/[id]/details',
+      params: { id: props.id }
+    }}>
+      <Image source={{ uri: props.cover_image }} style={{ width: 100, height: 150 }} />
+      <ThemedText>{props.title}</ThemedText>
+    </Link>
+  );
+};
+
+export default AnimeCard;
